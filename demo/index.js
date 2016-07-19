6476828fd08fb89cb8b1607947ca630ea514d675
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-07-19 14:25:00 +0200

    Initial commit

---
+++ demo/index.js
@@ -0,0 +1,462 @@
+function onLoad() {
+    // Cross-browser support:
+    navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.getUserMedia = function(arg, t, c) {
+            return navigator.mediaDevices.getUserMedia(arg).then(t).catch(c);
+        };
+    };
+
+    // Connection data:
+    var f = document.getElementById("login-form");
+
+    var a = undefined;
+    var username = undefined;
+    var roster = undefined;
+    var chatboxes = {}
+    var calls = {};
+
+    function login() {
+        a = artichoke({"rtc": {"iceServers": [{"urls": ["stun:46.101.163.186:3478", "turn:46.101.163.186:3478"],
+                                               "username": "test123",
+                                               "credential":"test456"}]},
+                       "url": f.elements[0].value,
+                       "apiKey": f.elements[1].value,
+                       "debug": true
+                      });
+
+        document.getElementById("login-box").style = "display: none;";
+        document.getElementById("call-container").style = "display: block";
+        document.getElementById("chatbox-container").style = "display: block";
+
+        a.onConnect = function() {
+            log("Connected to artichoke!");
+
+            a.onMessage("hello", function(m) {
+                username = m.username;
+                log("Connection ready for " + username + "!");
+
+                a.onMessage("call", function(m) {
+                    var peer = m.sender;
+                    switch(m.signal) {
+                    case "offer":
+                        log(peer + " is calling...");
+                        if(confirm(peer + " is calling, answer?")) {
+                            makeCall(peer).createLocalStream(function(stream) {
+                                a.call.answer(peer, m, stream);
+                            });
+                        }
+                        else {
+                            log("Rejecting call...");
+                            a.call.reject(peer);
+                        }
+                        break;
+
+                    case "answer":
+                        log(peer + " answered the call!");
+                        break;
+
+                    case "hangup":
+                        log(peer + " hang up, reason: " + m.body);
+                        removeCall(peer);
+                        break;
+
+                    default: break;
+                    }
+                });
+
+                a.onMessage("presence", function(m) {
+                    var status = m.status;
+                    var user = m.sender;
+                    var line = "User " + user + " is " + status + "!";
+                    if(user in chatboxes) chatboxes[user].receive(line);
+                    else log(line);
+                    roster.update(user, status);
+                });
+
+                a.onMessage("roster_add", function(m) {
+                    roster.add(m.user, "available"); // FIXME Actually implement proper status handling.
+                    log("User " + m.user + " added to roster.");
+                });
+
+                a.onMessage("roster_remove", function(m) {
+                    roster.remove(m.user);
+                    log("User " + m.user + " removed from roster.");
+                });
+
+                a.onMessage("roster", function(m) {
+                    var users = {}
+                    m.users.forEach(function (u) {
+                        users[u] = "available"; // FIXME Actually implement proper status handling.
+                    })
+                    roster.set(users);
+                    log("Roster: " + m.users);
+                });
+
+                function makeAddable(handle) {
+                    var b = document.createElement("button");
+                    b.innerHTML = handle;
+                    b.onclick = function() {
+                        if(handle != username) {
+                            log("Adding user " + handle + " to the roster.");
+                            a.roster.add(handle);
+                        }
+                    }
+                    return b;
+                }
+
+                a.onMessage("room", function(m) {
+                    makeChatbox(m.room).receive("Users currently in ", makeAddable(m.room), ": ", m.users.map(makeAddable));
+                });
+
+                a.onMessage("room_action", function(m) {
+                    var s = m.subject ? makeAddable(m.subject) : "the room";
+                    makeChatbox(m.room).receive("User ", makeAddable(m.originator), " ", m.action, " ", s, ".");
+                });
+
+                a.onMessage("message", function(m) {
+                    makeChatbox(m.room).receive("[" + m.sender + "] " + m.body)
+                });
+
+                a.onMessage("msg_received", function(m) {
+                    log("Received ack for message id: " + m.id);
+                });
+
+                a.onMessage("msg_delivered", function(m) {
+                    log("Message delivery ack for id: " + m.id);
+                });
+
+                makeChatboxControls("chatbox-controls");
+                roster = makeRoster("chatbox-roster");
+            });
+        }
+        a.connect();
+    }
+
+    function makeCall(peer) {
+        log("Creating a call object.");
+        var call = calls[peer];
+        if(call) return call;
+
+        var box = document.createElement("div")
+
+        var local_video = document.createElement("video")
+        local_video.className = "video-stream";
+        local_video.id = "local-video";
+        local_video.autoplay = true;
+        box.appendChild(local_video);
+
+        var remote_video = document.createElement("video")
+        remote_video.className = "video-stream";
+        remote_video.id = "remote-video";
+        remote_video.autoplay = true;
+        box.appendChild(remote_video);
+
+        var end = document.createElement("button");
+        end.innerHTML = "Hangup call";
+        end.onclick = function() {
+            log( "Ending call with " + peer + "...");
+            a.call.hangup(peer, "hangup");
+            removeCall(peer);
+        };
+        box.appendChild(end);
+
+        document.getElementById("call-container").appendChild(box);
+
+        var local_stream = undefined;
+        var remote_stream = undefined;
+
+        function showRemoteStream(stream) {
+            remote_stream = stream;
+            remote_video.src = window.URL.createObjectURL(stream);
+        }
+
+        function showLocalStream(stream) {
+            local_stream = stream;
+            local_video.src = window.URL.createObjectURL(stream);
+        }
+
+        function stopStreams() {
+            if(local_stream) {
+                if(local_stream.stop) local_stream.stop();
+                else local_stream.getTracks().map(function(t) { t.stop(); });
+                local_stream = undefined;
+                remote_stream = undefined;
+            };
+        }
+
+        function createLocalStream(onLocalStream) {
+            navigator.getUserMedia(
+                {"video": f.elements[3].checked,
+                 "audio": f.elements[2].checked},
+                function(stream) {
+                    log("Local stream started!");
+                    showLocalStream(stream);
+                    onLocalStream(stream);
+                }, function(error) {
+                    log("Could not start stream: " + error);
+                });
+        };
+
+        a.onRemoteStream = function(stream) {
+            log("Remote stream started!");
+            showRemoteStream(stream);
+        };
+
+        calls[peer] = {
+            box: box,
+            peer: peer,
+            showRemoteStream: showRemoteStream,
+            createLocalStream: createLocalStream,
+            stopStreams: stopStreams
+        };
+
+        return calls[peer];
+    }
+
+    function removeCall(peer) {
+        log("Removing a call object.");
+        var call = calls[peer];
+        if(call) {
+            call.stopStreams();
+            document.getElementById("call-container").removeChild(call.box);
+            delete calls[peer];
+        }
+    }
+
+    function makeRoster(id) {
+        log("Building the roster.");
+        var roster = {};
+
+        function regenRoster() {
+            var r = document.getElementById(id);
+            r.innerHTML = "";
+
+            // NOTE Can't use just for, since it'll overwrite u in each onclick.
+            var users = [];
+            for(var u in roster) users.push(u);
+            users.forEach(function(u) {
+                var chat = document.createElement("button");
+                chat.onclick = function() {
+                    log("Opening chat with " + u + "...");
+
+                    var name = "dm-" + [username, u].sort().join("-");
+                    var room = createRoom(name);
+                    a.room.join(room.id);
+                    a.room.invite(room.id, u);
+
+                    makePrivateChatbox(room.id);
+                }
+                chat.innerHTML = "Chat with " + u;
+                r.appendChild(chat);
+
+                if(roster[u] == "available") {
+                    var call = document.createElement("button");
+                    call.onclick = function() {
+                        log("Calling " + u + "...");
+                        var keys = [];
+                        for(var k in calls) { keys.push(k); }
+                        log(keys);
+
+                        if(keys.length == 0) {
+                            makeCall(u).createLocalStream(function(stream) {
+                                a.call.offer(u, stream);
+                            });
+                        }
+                        else if(confirm("You are arleady calling someone. Hang up that call?")) {
+                            keys.forEach(function(k) {
+                                a.call.hangup(k, "hangup");
+                                removeCall(k);
+                            });
+                            makeCall(u).createLocalStream(function(stream) {
+                                a.call.offer(u, stream);
+                            });
+                        }
+                    };
+                    call.innerHTML = "Call " + u;
+                    r.appendChild(call);
+                }
+
+                var remove = document.createElement("button");
+                remove.onclick = function() {
+                    log("Removing user " + u + " from roster.");
+                    a.roster.remove(u);
+                    delete roster[u];
+                    regenRoster()
+                }
+                remove.innerHTML = "Remove " + u;
+                r.appendChild(remove);
+            });
+        }
+
+        return {
+            set: function(users) {
+                roster = users;
+                regenRoster();
+            },
+
+            add: function(user, status) {
+                if(!roster[user]) {
+                    roster[user] = status;
+                    regenRoster();
+                }
+            },
+
+            remove: function(user) {
+                delete roster[user];
+                regenRoster();
+            },
+
+            update: function(user, status) {
+                if(roster[user]) {
+                    roster[user] = status;
+                    regenRoster();
+                }
+            }
+        }
+    }
+
+    function makeChatbox(room) {
+        log("Creating a chatbox object: " + room);
+        var chat = makeGenericChatbox(room);
+        chat.teardown = function() {
+            a.room.leave(room);
+        }
+        return chat;
+    }
+
+    function makePrivateChatbox(room) {
+        log("Creating a private chatbox object: " + room);
+        return makeGenericChatbox(room);
+    }
+
+    function makeGenericChatbox(room) {
+        var chat = chatboxes[room];
+        if(chat) return chat;
+
+        var box = document.createElement("div");
+
+        var id = room + "-chatbox";
+
+        box.className = "chatbox";
+        box.id = id;
+
+        var text = document.createElement("div");
+        text.className = "chatbox-textarea";
+        box.appendChild(text);
+
+        var p = document.createElement("p");
+        p.innerHTML = "Chatting with " + room + "..."
+        text.appendChild(p);
+
+        var controls = document.createElement("form");
+        var input = document.createElement("input");
+        input.type = "text";
+        controls.appendChild(input);
+
+        var send = document.createElement("input");
+        send.type = "submit";
+        send.value = "Send!";
+        controls.appendChild(send);
+
+        controls.onsubmit = function() {
+            receive("[" + username + "] " + input.value);
+            a.chat(room, input.value);
+            input.value = "";
+            return false;
+        };
+
+        var end = document.createElement("input");
+        end.type = "button";
+        end.value = "Close chat";
+        end.onclick = function() {
+            removeChatbox(room);
+        };
+        controls.appendChild(end);
+        box.appendChild(controls);
+        document.getElementById("chatbox-container").appendChild(box);
+
+        function receive() {
+            var p = document.createElement("p");
+
+            function append(element) {
+                if(element.appendChild != undefined) {
+                    p.appendChild(element);
+                }
+                else if(typeof element === "object") {
+                    element.forEach(function(e) {
+                        append(e);
+                    });
+                }
+                else {
+                    var c = document.createElement("span");
+                    c.innerHTML = element;
+                    p.appendChild(c);
+                }
+            }
+
+            for(var a in arguments) {
+                append(arguments[a]);
+            }
+
+            log("[" + room + "] " + p.innerHTML);
+            text.appendChild(p);
+            text.scrollTop = text.scrollHeight - text.clientHeight;
+        }
+
+        chatboxes[room] = {
+            id: id,
+            box: box,
+            receive: receive
+        };
+
+        return chatboxes[room];
+    }
+
+    function removeChatbox(room) {
+        log("Removing a chatbox object: " + room);
+        var chat = chatboxes[room];
+        document.getElementById("chatbox-container").removeChild(chat.box);
+        if(chat.teardown) chat.teardown();
+        delete chatboxes[room];
+    }
+
+    function makeChatboxControls(id) {
+        log("Creating a chatbox controls: " + id);
+        var box = document.createElement("form");
+        var room = document.createElement("input");
+        room.type = "text";
+        room.value = "#artichoke";
+        box.appendChild(room);
+
+        var join = document.createElement("input");
+        join.type = "submit";
+        join.value = "Join room";
+        box.appendChild(join);
+
+        box.onsubmit = function() {
+            try {
+                var r = createRoom(room.value);
+                a.room.join(r.id);
+            } catch(e) {
+                log("Error while creating a room: " + e);
+            }
+            return false;
+        };
+
+        document.getElementById(id).appendChild(box);
+    }
+
+    function createRoom(name) {
+        log("Creating a chat room: " + name);
+        return a.room.create(name);
+    }
+
+    function log(str) {
+        console.log(str);
+    }
+
+    // Enable login button:
+    document.getElementById("login").onclick = login;
+}
---
+++ src/artichoke.js
@@ -0,0 +1,265 @@
+function artichoke(config) {
+    logDebug("config: " + JSON.stringify(config));
+
+    // Cross-browser support:
+    var RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+
+    // Messages:
+    function Call(from, to, signal, payload) {
+        return {
+            "type": "call",
+            "sender": from,
+            "recipient": to,
+            "signal": signal.toLowerCase(),
+            "body": payload
+        };
+    };
+
+    function ChatRequest(room, body) {
+        return {
+            "type": "msg_request",
+            "room": room,
+            "body": body
+        };
+    };
+
+    function ChatDelivered(id, timestamp) {
+        return {
+            "type": "msg_delivered",
+            "id": id,
+            "timestamp": timestamp
+        };
+    };
+
+    function RoomCreate(name) {
+        return {
+            "type": "room_create",
+            "name": name
+        };
+    }
+
+    function RoomJoin(room) {
+        return {
+            "type": "room_join",
+            "room": room
+        };
+    };
+
+    function RoomLeave(room) {
+        return {
+            "type": "room_leave",
+            "room": room
+        };
+    };
+
+    function RoomInvite(room, user) {
+        return {
+            "type": "room_invite",
+            "room": room,
+            "user": user
+        };
+    };
+
+    function RosterAdd(user) {
+        return {
+            "type": "roster_add",
+            "user": user
+        };
+    };
+
+    function RosterRemove(user) {
+        return {
+            "type": "roster_remove",
+            "user": user
+        };
+    };
+
+    // Connection state:
+    var userId = config.apiKey; // FIXME Actually get it.
+    var pc = undefined;
+    var s = undefined;
+
+    var messageCallbacks = {};
+
+    // API:
+    var self = {
+        connect: connect,
+
+        call: {
+            offer: offer,
+            answer: answer,
+            reject: function(peer) { send(Call(userId, peer, "hangup", "rejected")); },
+            hangup: function(peer, reason) {
+                pc = reconnectRTC(pc);
+                send(Call(userId, peer, "hangup", reason));
+            }
+        },
+
+        chat: function(peer, body) { send(ChatRequest(peer, body)); },
+
+        room: {
+            create: function(name) { return post("http://" + pathcat(config.url, "api", "room", "create"), RoomCreate(name)); },
+            join: function(room) { send(RoomJoin(room)); },
+            leave: function(room) { send(RoomLeave(room)); },
+            invite: function(room, who) { send(RoomInvite(room, who)); }
+        },
+
+        roster: {
+            add: function(who) { send(RosterAdd(who)); },
+            remove: function(who) { send(RosterRemove(who)); }
+        },
+
+        onConnect: function() {}, // NOTE By default do nothing.
+        onError: function (e) { console.log(e); },
+        onMessage: function(type, callback) {
+            logDebug("Registered callback for message type: " + type);
+            messageCallbacks[type] = callback;
+        },
+
+        onRemoteStream: function() {} // NOTE By default do nothing.
+    };
+
+    function connect() {
+        var url = "ws://" + pathcat(config.url, "ws", config.apiKey);
+
+        console.log("Connecting to " + url);
+        s = new WebSocket(url);
+        s.binaryType = "arraybuffer";
+
+        s.onopen = function() {
+            logDebug("Connected to " + url);
+            self.onConnect();
+        };
+
+        s.onmessage = function(event) {
+            logDebug("Received: " + event.data);
+            var m = JSON.parse(event.data);
+
+            switch(m.type) {
+            case "call":
+                var peer = m.sender;
+                switch(m.signal) {
+                case "answer":
+                    pc.setRemoteDescription(new RTCSessionDescription({"type": "answer", "sdp": m.body}));
+                    pc.onicecandidate = onICE(userId, peer);
+                    break;
+
+                case "hangup":
+                    pc = reconnectRTC(pc);
+                    break;
+
+                case "candidate":
+                    pc.addIceCandidate(new RTCIceCandidate({"candidate": m.body, "sdpMid": "", "sdpMLineIndex": 0}));
+                    break;
+
+                default: break;
+                }
+                runCallback(m);
+                break;
+
+            case "message":
+                if(!m.delivered) send(ChatDelivered(m.id, Date.now()));
+                runCallback(m);
+                break;
+
+            case "hello":
+                runCallback(m);
+                break;
+
+            default:
+                runCallback(m);
+            }
+        };
+
+        pc = reconnectRTC();
+    }
+
+    function offer(peer, stream) {
+        pc.addStream(stream);
+
+        pc.createOffer(function(offer) {
+            pc.setLocalDescription(offer);
+            send(Call(userId, peer, "offer", offer.sdp));
+        }, logError);
+    }
+
+    function answer(peer, offer, stream) {
+        pc.setRemoteDescription(new RTCSessionDescription({"type": "offer", "sdp": offer.body}));
+        pc.onicecandidate = onICE(userId, peer);
+
+        pc.addStream(stream);
+
+        pc.createAnswer(function(answer) {
+            pc.setLocalDescription(answer);
+            send(Call(userId, peer, "answer", answer.sdp));
+        }, logError);
+    }
+
+    // Utils:
+    function reconnectRTC(old_pc) {
+        if(old_pc) old_pc.close();
+
+        var pc = new RTCPeerConnection(config.rtc);
+
+        var onstream = function(event) {
+            self.onRemoteStream(event.stream || event.streams[0]);
+        };
+
+        if(pc.ontrack === null) pc.ontrack = onstream;
+        else pc.onaddstream = onstream;
+
+        return pc;
+    }
+
+    function runCallback(m) {
+        if(m.type in messageCallbacks) {
+            logDebug("Runnig callback for message type: " + m.type);
+            return messageCallbacks[m.type](m)
+        }
+        else {
+            logDebug("Unhandled message: " + JSON.stringify(m));
+            self.onError({"type": "unhandled_message", "message": m});
+        }
+    }
+
+    function logError(error) {
+        console.log(error);
+    }
+
+    function logDebug(log) {
+        if(config.debug) console.log("DEBUG: " + log);
+    }
+
+    function onICE(sender, recipient) {
+        return function(event) {
+            if (event.candidate) {
+                send(Call(sender, recipient, "candidate", event.candidate.candidate));
+            }
+        };
+    }
+
+    function pathcat() {
+        var output = [];
+        for(var i in arguments) output.push(arguments[i]);
+        return output.join("/");
+    }
+
+    function send(obj) {
+        var json = JSON.stringify(obj);
+        s.send(json);
+        logDebug("Sent: " + json);
+    }
+
+    function post(url, obj) {
+        var json = JSON.stringify(obj);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, false);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("X-Api-Key", config.apiKey);
+        xhttp.send(json);
+        logDebug("POST: " + json);
+        return JSON.parse(xhttp.responseText);
+    }
+
+    return self;
+};
