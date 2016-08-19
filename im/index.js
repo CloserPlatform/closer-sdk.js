f0b2e43d42ee68c4a7d0c1dbcac0c0fe1735c27f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-08-19 15:15:08 +0200

    Update the IM demo to support new Call callbacks

--- im/index.js
+++ im/index.js
@@ -152,6 +152,7 @@ $(document).ready(function() {
         var panel = makePanel().append(callBox);
 
         var onAddCall = function() {};
+        var onHangup = function () {};
 
         room.getUsers().then(function(list) {
             var peer = list.users.filter(function(u) {
@@ -168,7 +169,7 @@ $(document).ready(function() {
                     hangup.removeClass("disabled");
 
                     onAddCall = function(c) {
-                        c.offer();
+                        c.join();
                     }
 
                     callBuilder(room, peer);
@@ -178,7 +179,7 @@ $(document).ready(function() {
 
             hangup = makeButton("btn-danger disabled", "Hangup!", function() {
                 if(!hangup.hasClass("disabled")) {
-                    calls[peer].hangup("hangup");
+                    onHangup();
                 }
             });
 
@@ -217,7 +218,10 @@ $(document).ready(function() {
                 call.addClass("disabled");
                 hangup.removeClass("disabled");
                 callBox.append(c.element);
-
+                onHangup = function() {
+                    c.leave("hangup");
+                    delete calls[c.call.id];
+                }
                 onAddCall(c);
             },
             removeCall: function() {
@@ -226,6 +230,7 @@ $(document).ready(function() {
                 callBox.html("");
 
                 onAddCall = function() {};
+                onHangup = function() {};
             },
             remove: function() {
                 chatbox.remove();
@@ -400,7 +405,7 @@ $(document).ready(function() {
         }
     }
 
-    function makeCall(call, peer, onTeardown) {
+    function makeCall(call, onTeardown) {
         console.log("Building a call object for: ", call);
 
         var localStream = undefined;
@@ -431,29 +436,15 @@ $(document).ready(function() {
             streams.show();
         });
 
-        call.onHangup(function(m) {
-            console.log("Received call hangup: ", m);
+        call.onLeft(function(m) {
+            console.log("User left the call: ", m);
             alert("Call ended, reason: " + m.reason);
             stopStreams();
+            call.leave("ended");
         });
 
-        call.onAnswer(function(m) {
-            console.log("Received call answer: ", m);
-        });
-
-        call.onOffer(function(m) {
-            console.log("Received call offer: ", m);
-            if(confirm(peer + " is calling, answer?")) {
-                createLocalStream(function(stream) {
-                    call.answer(m, stream).catch(function(error) {
-                        console.log("Answer creation failed: ", error);
-                    });
-                });
-            } else {
-                console.log("Rejecting call...");
-                call.reject(m);
-                stopStreams();
-            }
+        call.onJoined(function(m) {
+            console.log("User joined the call: ", m);
         });
 
         function stopStreams() {
@@ -467,34 +458,39 @@ $(document).ready(function() {
         }
 
         return {
+            call: call,
             element: streams,
-            offer: function() {
+            createStream: function() {
                 createLocalStream(function(stream) {
-                    call.offer(stream).catch(function(error) {
-                        console.log("Offer creation failed: ", error);
-                    });
+                    call.addStream(stream);
                 });
             },
-            hangup: function(reason) {
-                call.hangup(reason);
+            join: function() {
+                createLocalStream(function(stream) {
+                    call.join(stream);
+                });
+            },
+            leave: function(reason) {
+                call.leave(reason);
                 stopStreams();
             },
         }
     }
 
-    function addCall(user, room, call) {
-        var box = makeCall(call, user, function() {
+    function addCall(room, call) {
+        var box = makeCall(call, function() {
             chatboxes[room.id].removeCall();
-            delete calls[user];
+            delete calls[call.id];
         });
-        calls[user] = box;
+        calls[call.id] = box;
         chatboxes[room.id].addCall(box);
     }
 
     function callBuilder(session) {
         return function(room, user) {
             session.chat.createCall([user]).then(function(call) {
-                addCall(user, room, call);
+                addCall(room, call);
+                calls[call.id].createStream();
                 switchers[room.id].switchTo();
             }).catch(function(error) {
                 console.log("Creating a call failed: ", error);
@@ -548,13 +544,21 @@ $(document).ready(function() {
                 });
 
                 session.chat.onCall(function(m) {
-                    session.chat.createDirectRoom(m.creator).then(function(room) {
-                        addRoom(room, session);
-                        addCall(m.creator, room, m.call);
-                        switchers[room.id].switchTo();
-                    }).catch(function(error) {
-                        console.log("Creating direct room failed: ", error);
-                    });
+                    console.log("Received call offer: ", m);
+
+                    if(confirm(m.user + " is calling, answer?")) {
+                        session.chat.createDirectRoom(m.user).then(function(room) {
+                            addRoom(room, session);
+                            addCall(room, m.call);
+                            calls[m.call.id].join();
+                            switchers[room.id].switchTo();
+                        }).catch(function(error) {
+                            console.log("Creating direct room failed: ", error);
+                        });
+                    } else {
+                        console.log("Rejecting call...");
+                        m.call.reject(m);
+                    }
                 });
             });
 
