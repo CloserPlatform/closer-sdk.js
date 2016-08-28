36a554632b5a110b302cda8958a86473e035d98b
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-08-28 10:20:48 +0200

    Make typings a local dependency

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Ratel JavaScript SDK
-For typing you'll need 'npm install typings --global'
 Building:
 
 ```
-typings install
 npm install
 npm run build
 ```
--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/webrtc/mediastream/index.d.ts" />
-/// <reference path="globals/webrtc/rtcpeerconnection/index.d.ts" />
