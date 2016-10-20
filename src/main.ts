ac391f0f75b5823c8161977a4d57b7f364cbf041
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-10-20 17:05:52 +0200

    Export various useful stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
 /// <reference types="es6-shim" />
 /// <reference types="webrtc" />
 
+// Main entry points:
 export * from "./auth";
+
+// Some useful types:
+export { Artichoke as Chat } from "./artichoke";
+export { Call, DirectCall } from "./call";
+export { Config } from "./config";
+export { Media } from "./media";
+export { Message } from "./message";
+export { Action, Archivable, Metadata} from "./protocol";
+export { DirectRoom, Room } from "./room";
+export { Session } from "./session";
+
+// Other useful crap:
+import * as api from "./api";
+import * as events from "./events";
+import * as logger from "./logger";
+import * as protocol from "./protocol";
+
+export { api, events, logger, protocol };
