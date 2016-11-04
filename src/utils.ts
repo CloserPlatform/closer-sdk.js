2b62fc76f4055168d29c6e524db9e8c2bbea19bb
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-11-04 16:39:12 +0100

    Add deep copy function

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,21 @@
 // Various utilities.
 
 export interface TransferFunction<T, U> {
-    (arg: T): U;
+  (arg: T): U;
 }
 
 export function wrapPromise<T, U>(promise: Promise<T | Array<T>>, fun: TransferFunction<T, U>): Promise<U | Array<U>> {
-    return new Promise<U | Array<U>>(function(resolve, reject) {
-        promise.then(function(obj) {
-            if (Array.isArray(obj)) {
-                resolve((obj as Array<T>).map(fun));
-            } else {
-                resolve(fun(obj as T));
-            }
-        }).catch(reject);
-    });
+  return new Promise<U | Array<U>>(function(resolve, reject) {
+    promise.then(function(obj) {
+      if (Array.isArray(obj)) {
+        resolve((obj as Array<T>).map(fun));
+      } else {
+        resolve(fun(obj as T));
+      }
+    }).catch(reject);
+  });
+}
+
+export function deepcopy<T>(obj: T): T {
+  return JSON.parse(JSON.stringify(obj)); // FIXME Deal with it.
 }
