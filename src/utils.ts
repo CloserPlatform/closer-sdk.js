e910cbe6b19bae6153eff0645a1f480d620d6b18
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-11-15 13:45:16 +0100

    Refactor promise handling

--- src/utils.ts
+++ src/utils.ts
@@ -5,14 +5,12 @@ export interface TransferFunction<T, U> {
 }
 
 export function wrapPromise<T, U>(promise: Promise<T | Array<T>>, fun: TransferFunction<T, U>): Promise<U | Array<U>> {
-  return new Promise<U | Array<U>>(function(resolve, reject) {
-    promise.then(function(obj) {
-      if (Array.isArray(obj)) {
-        resolve((obj as Array<T>).map(fun));
-      } else {
-        resolve(fun(obj as T));
-      }
-    }).catch(reject);
+  return promise.then(function(obj) {
+    if (Array.isArray(obj)) {
+      return (obj as Array<T>).map(fun);
+    } else {
+      return fun(obj as T);
+    }
   });
 }
 
