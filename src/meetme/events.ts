a5f148b031b47ff2f0abeae6219e0ebcd69a3c8f
Jacek Malec <jacek.malec@ratel.io>
2017-09-25 10:55:11 +0200

    Meetme protocol

---
+++ src/meetme/events.ts
@@ -0,0 +1,64 @@
+import {Codec, EventEntity} from "../codec";
+import {ID} from "../protocol/protocol";
+
+export interface MeetmeEvent extends EventEntity {
+}
+
+export interface PresencedUpdated extends MeetmeEvent {
+  user: ID;
+  presence: Presence;
+  timestamp: number;
+}
+
+export enum Presence {
+  AVAILABLE = "available",
+  AWAY = "away",
+  UNAVAILABLE = "unavailable"
+}
+
+export interface Typing extends MeetmeEvent {
+  room: ID;
+  user: ID;
+  timestamp: number;
+}
+
+export interface MessageSent extends MeetmeEvent {
+  room: ID;
+  sender: ID;
+  timestamp: number;
+  body: string;
+}
+
+export interface RoomJoined extends MeetmeEvent {
+  room: ID;
+  user: ID;
+  timestamp: number;
+}
+
+export interface RoomLeft extends MeetmeEvent {
+  room: ID;
+  user: ID;
+  timestamp: number;
+}
+
+export interface Error extends MeetmeEvent {
+  reason: string;
+}
+
+export enum MeetmeEventType {
+  PRESENCE_UPDATED = "presence_updated",
+  TYPING = "room_typing",
+  MESSAGE_SENT = "room_message_sent",
+  ROOM_JOINED = "room_joined",
+  ROOM_LEFT = "room_left",
+  ERROR = "error"
+}
+
+export const codec: Codec<MeetmeEvent> = {
+  decode: (data: string) => JSON.parse(data),
+  encode: (value: MeetmeEvent) => JSON.stringify(value)
+};
+
+export function error(reason: string): Error {
+  return {type: MeetmeEventType.ERROR, reason};
+}
--- src/meetme/meetme.ts
+++ src/meetme/meetme.ts
@@ -1,41 +1,68 @@
-import { ApiKey } from "../auth";
-import { Callback, EventHandler } from "../events";
-import { JSONWebSocket } from "../jsonws";
+import {ApiKey} from "../auth";
+import {Callback, EventHandler} from "../events";
+import {JSONWebSocket} from "../jsonws";
 import * as logger from "../logger";
-import { Logger } from "../logger";
-import { Config } from "./config";
-import { codec, MeetmeEvent } from "./event";
+import {Logger} from "../logger";
+import {ID} from "../protocol/protocol";
+import {Config} from "./config";
+import * as events from "./events";
+import {MeetmeEventType} from "./events";
 
 export class MeetmeWebsocket {
   protected log: Logger;
 
   private url: string;
+  private orgId: ID;
 
-  private socket: JSONWebSocket<MeetmeEvent>;
-  private events: EventHandler<MeetmeEvent>;
+  private socket: JSONWebSocket<events.MeetmeEvent>;
+  private events: EventHandler<events.MeetmeEvent>;
 
-  constructor(apiKey: ApiKey, config: Config) {
+  constructor(apiKey: ApiKey, config: Config, orgId: ID) {
     this.log = config.debug ? logger.debugConsole : logger.devNull;
 
     this.log("Configuration: " + JSON.stringify(config));
-    this.events = new EventHandler(this.log, codec);
-    this.socket = new JSONWebSocket(this.log, codec);
+    this.events = new EventHandler(this.log, events.codec);
+    this.socket = new JSONWebSocket(this.log, events.codec);
 
     let host = config.ws.hostname + (config.ws.port === "" ? "" : ":" + config.ws.port);
     this.url = [config.ws.protocol, "//", host, "/ws/", apiKey].join("");
+
+    this.orgId = orgId;
   }
 
   connect() {
     this.socket.connect(this.url);
+    this.socket.onEvent(this.notify);
   }
 
   disconnect() {
     this.socket.disconnect();
   }
 
-  onEvent(callback: Callback<MeetmeEvent>) {
-    this.socket.onDisconnect(callback);
-    this.socket.onError(callback);
-    this.socket.onEvent(callback);
+  onPresenceUpdated(callback: Callback<events.PresencedUpdated>) {
+    this.events.onEvent(MeetmeEventType.PRESENCE_UPDATED, callback);
+  }
+
+  onTyping(callback: Callback<events.Typing>) {
+    this.events.onEvent(MeetmeEventType.TYPING, callback);
+  }
+
+  onMessage(callback: Callback<events.MessageSent>) {
+    this.events.onEvent(MeetmeEventType.MESSAGE_SENT, callback);
+  }
+
+  onJoined(callback: Callback<events.RoomJoined>) {
+    this.events.onEvent(MeetmeEventType.ROOM_JOINED, callback);
+  }
+
+  onLeft(callback: Callback<events.RoomLeft>) {
+    this.events.onEvent(MeetmeEventType.ROOM_LEFT, callback);
+  }
+
+  private notify(event: events.MeetmeEvent): void {
+    this.events.notify(event, (e) => {
+        this.events.notify(events.error("Unhandled " + e.type));
+      }
+    );
   }
 }
