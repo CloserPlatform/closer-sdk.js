dc5520d7660ffc0ee3648561c806a0f5db86188c
Jakub Pęksa <Jakub Pksa>
2018-02-12 10:12:57 +0100

    remove header array, instead create new instance each time someone request it

---
+++ src/api-headers.ts
@@ -0,0 +1,31 @@
+import { HeaderValue } from "./api";
+import { ApiKey } from "./auth";
+import * as proto from "./protocol/protocol";
+
+export class ApiHeaders {
+
+  private readonly deviceIdKey = "X-Device-Id";
+  private readonly apiKeyKey = "X-Api-Key";
+
+  private _deviceId: proto.ID;
+  private _apiKey: ApiKey;
+
+  get deviceId(): proto.ID {
+    return this._deviceId;
+  }
+
+  set deviceId(value: proto.ID) {
+    this._deviceId = value;
+  }
+
+  get apiKey(): ApiKey {
+    return this._apiKey;
+  }
+
+  set apiKey(value: ApiKey) {
+    this._apiKey = value;
+  }
+
+  public getHeaders = (): HeaderValue[] =>
+    [new HeaderValue(this.apiKeyKey, this._apiKey), new HeaderValue(this.deviceIdKey, this._deviceId)]
+}
