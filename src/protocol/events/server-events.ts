bf23085813324b029ee1907260d9441f611a43ab
Bartosz Szmit <bartosz.szmit@ratel.io>
2018-04-13 10:24:24 +0200

    Add server-events to protocol

---
+++ src/protocol/events/server-events.ts
@@ -0,0 +1,29 @@
+import { DomainEvent } from "./domain-event";
+
+export namespace serverEvents {
+  export class Hello implements DomainEvent {
+    static readonly tag: string = "hello";
+
+    constructor(deviceId: string, timestamp: number, heartbeatTimeout: number) {
+      this.deviceId = deviceId;
+      this.timestamp = timestamp;
+      this.heartbeatTimeout = heartbeatTimeout;
+    }
+
+    readonly deviceId: string;
+    readonly timestamp: number;
+    readonly heartbeatTimeout: number;
+    readonly tag = Hello.tag;
+  }
+
+  export class OutputHeartbeat implements DomainEvent {
+    static readonly tag: string = "output_heartbeat";
+
+    constructor(timestamp: number) {
+      this.timestamp = timestamp;
+    }
+
+    readonly tag = OutputHeartbeat.tag;
+    readonly timestamp: number;
+  }
+}
