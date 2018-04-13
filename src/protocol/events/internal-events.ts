4ed29ca28f1ac438931125bf79d65c2aebcd4bb6
Bartosz Szmit <bartosz.szmit@ratel.io>
2018-04-13 16:41:48 +0200

    Refactor internal-events.ts

--- src/protocol/events/internal-events.ts
+++ src/protocol/events/internal-events.ts
@@ -9,6 +9,14 @@ export namespace internalEvents {
 
   export class WebsocketDisconnected implements DomainEvent {
     static readonly tag = "websocket_disconnected";
+
+    constructor(code: number, reason: string) {
+      this.code = code;
+      this.reason = reason;
+    }
+
+    readonly code: number;
+    readonly reason: string;
     readonly tag: string = WebsocketDisconnected.tag;
   }
 
