ffe0186a37e8addc229436bc6b41db1ffa653c8c
Bartosz Szmit <bartosz.szmit@ratel.io>
2018-05-10 10:05:54 +0200

    Fix EventsSpec test

--- src/events.spec.ts
+++ src/events.spec.ts
@@ -1,48 +1,55 @@
 import { EventHandler } from "./events";
 import { log } from "./fixtures.spec";
-import { Error, RoomMark } from "./protocol/events";
-import { codec, error, eventTypes, marked } from "./protocol/wire-events";
+import { decoder } from "./protocol/events/domain-event";
+import { errorEvents } from "./protocol/events/error-events";
+import { roomEvents } from "./protocol/events/room-events";
 import { randomUUID } from "./utils";
+import MarkSent = roomEvents.MarkSent;
 
-interface ErrorWithCause extends Error {
+class ErrorWithCause extends errorEvents.Error {
+  constructor(reason: string, cause: boolean) {
+    super(reason);
+    this.cause = cause;
+  }
   cause: boolean;
 }
 
-function msg(id: string): RoomMark {
-  return marked(id, "123", Date.now());
+function msg(id: string): MarkSent {
+  return new roomEvents.MarkSent(id, "123", Date.now());
 }
 
 describe("Event Handler", () => {
-  let events;
+  let events: EventHandler;
 
   beforeEach(() => {
-    events = new EventHandler(log, codec);
+    events = new EventHandler(log, decoder);
   });
 
   it("should allow defining & invoking error handlers", () => {
     let ok = true;
 
-    events.onEvent(eventTypes.ERROR, (error: ErrorWithCause) => ok = error.cause);
+    events.onEvent(errorEvents.Error.tag, (error: ErrorWithCause) => ok = error.cause);
     expect(ok).toBe(true);
-    events.notify(error("Dun goofed", false));
+    events.notify(new ErrorWithCause("Dun goofed", false));
     expect(ok).toBe(false);
-    events.notify(error("j/k", true));
+    events.notify(new ErrorWithCause("j/k", true));
     expect(ok).toBe(true);
   });
 
   it("should run error handler on unhandled event", () => {
     let ok = false;
 
-    events.onEvent(eventTypes.ERROR, (error: Error) => ok = true);
+    events.onEvent(errorEvents.Error.tag, (error: ErrorWithCause) => ok = true);
     expect(ok).toBe(false);
-    events.notify({ type: "unhandled" }, () => events.notify(error("Unhandled")));
+    events.notify({ tag: "unhandled", __discriminator__: "domainEvent" },
+      () => events.notify(new errorEvents.Error("Unhandled")));
     expect(ok).toBe(true);
   });
 
   it("should allow defining event handlers", () => {
     let ok = 0;
 
-    events.onEvent(eventTypes.ROOM_MARKED, (msg: RoomMark) => ok++);
+    events.onEvent(roomEvents.MarkSent.tag, (msg: roomEvents.MarkSent) => ok++);
     expect(ok).toBe(0);
 
     [1, 2, 3, 4, 5].forEach((i) => {
@@ -55,8 +62,8 @@ describe("Event Handler", () => {
     let first = 0;
     let second = 0;
 
-    events.onEvent(eventTypes.ROOM_MARKED, (msg: RoomMark) => first++);
-    events.onEvent(eventTypes.ROOM_MARKED, (msg: RoomMark) => second++);
+    events.onEvent(roomEvents.MarkSent.tag, (msg: roomEvents.MarkSent) => first++);
+    events.onEvent(roomEvents.MarkSent.tag, (msg: roomEvents.MarkSent) => second++);
 
     [1, 2, 3, 4, 5].forEach((i) => events.notify(msg(i.toString())));
 
@@ -67,7 +74,7 @@ describe("Event Handler", () => {
   it("should allow defining concrete event handlers", () => {
     let ok = "0";
 
-    events.onConcreteEvent(eventTypes.ROOM_MARKED, "3", randomUUID(), (msg: RoomMark) => ok = msg.id);
+    events.onConcreteEvent(roomEvents.MarkSent.tag, "3", randomUUID(), (msg: roomEvents.MarkSent) => ok = msg.roomId);
 
     [1, 2, 3, 4, 5].forEach((i) => events.notify(msg(i.toString())));
 
@@ -78,8 +85,8 @@ describe("Event Handler", () => {
     let first = false;
     let second = false;
 
-    events.onConcreteEvent(eventTypes.ROOM_MARKED, "3", randomUUID(), (msg: RoomMark) => first = true);
-    events.onConcreteEvent(eventTypes.ROOM_MARKED, "1", randomUUID(), (msg: RoomMark) => second = true);
+    events.onConcreteEvent(roomEvents.MarkSent.tag, "3", randomUUID(), (msg: roomEvents.MarkSent) => first = true);
+    events.onConcreteEvent(roomEvents.MarkSent.tag, "1", randomUUID(), (msg: roomEvents.MarkSent) => second = true);
 
     [1, 2, 3, 4, 5].forEach((i) => events.notify(msg(i.toString())));
 
@@ -91,8 +98,8 @@ describe("Event Handler", () => {
     let first = false;
     let second = 0;
 
-    events.onConcreteEvent(eventTypes.ROOM_MARKED, "3", randomUUID(), (msg: RoomMark) => first = true);
-    events.onEvent(eventTypes.ROOM_MARKED, (msg: RoomMark) => second++);
+    events.onConcreteEvent(roomEvents.MarkSent.tag, "3", randomUUID(), (msg: roomEvents.MarkSent) => first = true);
+    events.onEvent(roomEvents.MarkSent.tag, (msg: roomEvents.MarkSent) => second++);
 
     [1, 2, 3, 4, 5].forEach((i) => events.notify(msg(i.toString())));
 
@@ -101,12 +108,12 @@ describe("Event Handler", () => {
   });
 
   it("onConcreteEvent() should be equivalent to onEvent() with id assertion", () => {
-    let first: RoomMark = undefined;
-    let second: RoomMark = undefined;
+    let first: roomEvents.MarkSent = undefined;
+    let second: roomEvents.MarkSent = undefined;
 
-    events.onConcreteEvent(eventTypes.ROOM_MARKED, "3", randomUUID(), (msg: RoomMark) => first = msg);
-    events.onEvent(eventTypes.ROOM_MARKED, (msg: RoomMark) => {
-      if (msg.id === "3") {
+    events.onConcreteEvent(roomEvents.MarkSent.tag, "3", randomUUID(), (msg: roomEvents.MarkSent) => first = msg);
+    events.onEvent(roomEvents.MarkSent.tag, (msg: roomEvents.MarkSent) => {
+      if (msg.roomId === "3") {
         second = msg;
       }
     });
@@ -114,6 +121,6 @@ describe("Event Handler", () => {
     [1, 2, 3, 4, 5].forEach((i) => events.notify(msg(i.toString())));
 
     expect(first).toBe(second);
-    expect(first.id).toBe("3");
+    expect(first.roomId).toBe("3");
   });
 });
