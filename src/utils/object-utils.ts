21853cdb217705e23515137ef46f0f0be726706a
Mariusz Bełtowski <mariusz.beltowski@anymind.com>
2020-08-21 10:30:02 +0200

    Remove unused code

--- src/utils/object-utils.ts
+++ src/utils/object-utils.ts
@@ -10,8 +10,4 @@ export class ObjectUtils {
     // tslint:disable:no-unsafe-any
     return JSON.parse(JSON.stringify(obj)); // FIXME Deal with it.
   }
-
-  public static mapDefined<T, Z>(maybeValue: null | undefined | T, fn: (value: T) => Z): Z | undefined {
-    return maybeValue ? fn(maybeValue) : undefined;
-  }
 }
--- test/unit/utils/object-utils.spec.ts
+++ test/unit/utils/object-utils.spec.ts
@@ -18,22 +18,4 @@ describe('ObjectUtils', () => {
     obj.bar.baz = testVal3;
     expect(cpy.bar.baz).toEqual(testVal2);
   });
-
-  it('mapDefined should apply function on a defined value', () => {
-    const value = 'string';
-
-    const fn = (v: string): string => v.toUpperCase();
-
-    const result = ObjectUtils.mapDefined(value, fn);
-
-    expect(result).toBe(fn(value));
-  });
-
-  it('mapDefined should not apply function on undefined', () => {
-    const value = undefined;
-
-    const result = ObjectUtils.mapDefined(value, v => String(v).toUpperCase());
-
-    expect(result).toBe(value);
-  });
 });
